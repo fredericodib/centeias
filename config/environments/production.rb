768c19a6e26b4cd3fff67b0b73b7e8867d7610f5
CrimsonCrown <arthurscouto@outlook.com>
2018-10-14 17:55:09 -0300

    adiciona mailer e arquivos com logica de mailer para teste

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,14 @@
+config.action_mailer.delivery_method = :smtp
+# SMTP settings for gmail
+config.action_mailer.smtp_settings = {
+ :address              => "smtp.gmail.com",
+ :port                 => 587,
+ :user_name            => ENV['gmail_username'],
+ :password             => ENV['gmail_password'],
+ :authentication       => "plain",
+:enable_starttls_auto => true
+}
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
