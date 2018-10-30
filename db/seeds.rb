160e9e96069bfb576d4dc06f1a5c514b3061cff8
claudiosegala <claudiosegalafilho@gmail.com>
2018-10-30 11:26:49 -0300

    an seeds example to implement later

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,143 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
+# User.new(
+# 	{ # Admin
+# 		:name => "Admin",
+# 		:email => "admin@centeias.com",
+# 		:password => "saladesituacao"
+# 	}
+# )
 
+# Folder.new([
+# 	{
+# 		:name => "Documentação de Uso",
+# 		:description => "Processos que explicam como funciona o uso desse sistema",
+# 		:created_by => 1 # id of the user which created
+# 	},
+# 	{
+# 		:name => "Boletins",
+# 		:description => "Processos para a criação de boletins para a sala de situação da Centeias",
+# 		:created_by => 1 # id of the user which created
+# 	},
+# 	{
+# 		:name => "Banco de Dados",
+# 		:description => "Como criar e alimentar um repositório de bancos de dados de interesse à saúde para disponibilização para a comunidade da Faculdade de Ciências da Saúde da Universidade de Brasília.",
+# 		:created_by => 1 # id of the user which created
+# 	},
+# 	{
+# 		:name => "Notícias",
+# 		:description => "Desenvolver a atividade de detecção de notícias de eventos de interesse à saúde.",
+# 		:created_by => 1 # id of the user which created
+# 	}
+# ])
+
+# Article.new([
+# 	{
+# 		:name => "Introdução",
+# 		:text => (
+# 			""/
+# 		),
+# 		:folder_id => 1,
+# 		:created_by => 1
+# 	},
+# 	{
+# 		:name => "Estrutura",
+# 		:text => (
+# 			""/
+# 		),
+# 		:folder_id => 1,
+# 		:created_by => 1
+# 	},
+# 	{
+# 		:name => "Acesso",
+# 		:text => (
+# 			""/
+# 		),
+# 		:folder_id => 1,
+# 		:created_by => 1
+# 	},
+# 	{
+# 		:name => "Monitoramento de Eventos",
+# 		:text => (
+# 			""/
+# 		),
+# 		:folder_id => 2,
+# 		:created_by => 1
+# 	},
+# 	{
+# 		:name => ,"Epidemiológico"
+# 		:text => (
+# 			""/
+# 		),
+# 		:folder_id => 2,
+# 		:created_by => 1
+# 	},
+# 	{
+# 		:name => "Repositório",
+# 		:text => (
+# 			""/
+# 		),
+# 		:folder_id => 3,
+# 		:created_by => 1
+# 	},
+# 	{
+# 		:name => "Clipping",
+# 		:text => (
+# 			""/
+# 		),
+# 		:folder_id => 4,
+# 		:created_by => 1
+# 	},
+# 	{
+# 		:name => "Busca",
+# 		:text => (
+# 			""/
+# 		),
+# 		:folder_id => 4,
+# 		:created_by => 1
+# 	}
+# ])
+
+# Subarticle.new([
+# 	{
+# 		:name => "Markdown",
+# 		:text => (
+# 			""/
+# 		),
+# 		:article_id => 1,
+# 		:created_by => 1
+# 	},
+# 	{
+# 		:name => "Gerenciamento de Pastas",
+# 		:text => (
+# 			""/
+# 		),
+# 		:article_id => 2,
+# 		:created_by => 1
+# 	},
+# 	{
+# 		:name => "Gerenciamento de Artigos",
+# 		:text => (
+# 			""/
+# 		),
+# 		:article_id => 2,
+# 		:created_by => 1
+# 	},
+# 	{
+# 		:name => "Gerenciamento de Sub-Artigos",
+# 		:text => (
+# 			""/
+# 		),
+# 		:article_id => 2,
+# 		:created_by => 1
+# 	},
+# 	{
+# 		:name => "Gerenciamento de Pastas",
+# 		:text => (
+# 			""/
+# 		),
+# 		:article_id => 2,
+# 		:created_by => 1
+# 	}
+# ])
