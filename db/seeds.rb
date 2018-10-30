b22fd1c47f366b2a2bc94b0f83923af716e20211
claudiosegala <claudiosegalafilho@gmail.com>
2018-10-30 20:13:40 -0300

    Admin user on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-# User.new(
-# 	{ # Admin
-# 		:name => "Admin",
-# 		:email => "admin@centeias.com",
-# 		:password => "saladesituacao"
-# 	}
-# )
+User.new(
+	{ # Admin
+		:name => "Admin",
+		:email => "admin@centeias.com",
+		:password => "saladesituacao"
+	}
+).save()
 
 # Folder.new([
 # 	{
