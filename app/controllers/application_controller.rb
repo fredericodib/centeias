4965f5dbec4b3c7ce0738a6c600209fd4be218c8
Gabriel Almeida <g.almeidaa.campos@gmail.com>
2018-10-31 23:10:01 -0300

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  # rescue_from CanCan::AccessDenied do |exception|
-  #   redirect_to root_url, :alert => 'Você não é autorizado a realizar esta ação.'
-  # end
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => 'Você não é autorizado a realizar esta ação.'
+  end
 end
