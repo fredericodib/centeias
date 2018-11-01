6fa67e12105571c9aebbee7aebbb8cc8fea48941
Gabriel Almeida <g.almeidaa.campos@gmail.com>
2018-10-31 22:15:22 -0300

    authorization feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
 	before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+	load_and_authorize_resource
 
 	def new
 		@article = Article.new
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181030222830) do
+ActiveRecord::Schema.define(version: 20181101004559) do
 
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 20181030222830) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.boolean "admin_flag"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
