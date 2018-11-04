b1012be6b9d1c1cdbe29ded66a022dbef29d458d
claudiosegala <claudiosegalafilho@gmail.com>
2018-11-04 14:51:11 -0200

    adjusting usage of requests

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -6,6 +6,9 @@ class RequestsController < ApplicationController
   end
 
   def show
+    @user = User.find_by(id: @request.users_id)
+    @article = Article.find_by(id: @request.articles_id)
+    @sub_article = SubArticle.find_by(id: @request.sub_articles_id)
   end
 
   def new
@@ -16,17 +19,28 @@ class RequestsController < ApplicationController
   end
 
   def create
-    @request = Request.new(request_params)
+    request = params[:request]
+    articles_id = request[:articles_id]
+    sub_articles_id = request[:sub_articles_id]
 
-    respond_to do |format|
-      if @request.save
-        format.html { redirect_to @request, notice: 'Request was successfully created.' }
-        format.json { render :show, status: :created, location: @request }
-      else
-        format.html { render :new }
-        format.json { render json: @request.errors, status: :unprocessable_entity }
-      end
+    if articles_id == 0 and sub_articles_id == 0
+			redirect_to folders_path()
     end
+
+    @request = Request.new(request_params)
+    @request.users_id = current_user.id
+    @request.articles_id = articles_id
+    @request.sub_articles_id = sub_articles_id
+
+    if @request.save
+      if @request.articles_id
+        redirect_to article_path(articles_id)
+      elsif @request.sub_articles_id
+        redirect_to sub_article_path(sub_articles_id)
+      end
+    else
+			redirect_to new_request_path(articles_id, sub_articles_id)
+		end
   end
 
   def update
